1fe59a9ff19b75b451c45c0a19918acb162b635b
sooyeon-kr <sooyeon7027@naver.com>
2024-10-27 20:02:16 +0900

    feat: 메뉴 flex로 변경 및 아이콘 추가

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,6 +1,7 @@
-import { useEffect } from "react";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
-import IconSetting from "@/assets/icon-setting.svg?react";
+import { useEffect } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import IconSetting from '@/assets/icon-setting.svg?react';
+import IconMenu from '/public/icon-menu.webp';
 export default function Layout() {
   const location = useLocation();
   const navigate = useNavigate();
@@ -21,28 +22,31 @@ export default function Layout() {
         </main>
         <div className="nav h-12 bg-red-50 w-full max-w-[576px] fixed bottom-0 flex items-center">
           <div className="flex flex-1 gap-4 items-center text-center p-4">
-            <p className="w-14 leading-snug" onClick={() => navigate("/")}>
+            <p className="flex-1 leading-snug" onClick={() => navigate('/')}>
               커뮤니티
             </p>
-            <p className="w-14 leading-snug" onClick={() => navigate("/rank")}>
+            <p
+              className="flex-1 leading-snug"
+              onClick={() => navigate('/rank')}
+            >
               주주클럽 랭킹
             </p>
             <p
-              className="w-14 leading-snug"
-              onClick={() => navigate("/profile")}
+              className="flex-1 leading-snug"
+              onClick={() => navigate('/profile')}
             >
               프로필
             </p>
             <p
-              className="w-14 leading-snug"
-              onClick={() => navigate("/lounge")}
+              className="flex-1 leading-snug"
+              onClick={() => navigate('/lounge')}
             >
               주주클럽 라운지
             </p>
-            <IconSetting />
           </div>
-          <div className="w-12">
-            <p>메뉴</p>
+          <div className="w-12 h-full flex flex-col items-center justify-center bg-blue-500 text-white">
+            <img src={IconMenu} alt="메뉴 아이콘" className="w-6 h-6 mb-1" />
+            <p className="text-xs">메뉴</p>
           </div>
         </div>
       </div>
