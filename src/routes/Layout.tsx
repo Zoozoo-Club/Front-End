3136f469ca543a24aa6f2d0a21d3744a882951c5
Joonwoo <liarcrown@naver.com>
2024-10-28 17:33:06 +0900

    design: 디자인 오류 수정

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet, useNavigate } from "react-router-dom";
-
+import { CiMenuBurger } from "react-icons/ci";
 import {
   useAuthStore,
   useLoginModalStore,
@@ -22,7 +22,7 @@ export default function Layout() {
   };
 
   return (
-    <div className="w-full h-full overflow-x-hidden overflow-y-scroll">
+    <div className="w-full h-full overflow-x-hidden overflow-y-scroll scroll">
       <div className="fixed w-full h-full overflow-hidden z-0">
         <div
           className="bg-cover bg-center w-full h-full fixed z-[-1]"
@@ -30,7 +30,7 @@ export default function Layout() {
         ></div>
       </div>
       <div className="max-w-xl w-xl h-screen-small relative z-1 m-auto flex flex-col items-center justify-between gap-6">
-        <main className="flex-1 w-full h-full overflow-scroll bg-white flex flex-col">
+        <main className="flex-1 w-full h-full overflow-scroll bg-white flex flex-col scroll">
           <Outlet />
         </main>
         <div className="nav h-12 bg-white w-full max-w-[576px] fixed bottom-0 flex items-center">
@@ -61,11 +61,7 @@ export default function Layout() {
             </p>
           </div>
           <div className="w-12 h-full flex flex-col items-center justify-center bg-blue-500 text-white">
-            <img
-              src="/icon-menu.webp?url"
-              alt="메뉴 아이콘"
-              className="w-6 h-6 mb-1"
-            />
+            <CiMenuBurger/>
             <p className="text-xs">메뉴</p>
           </div>
         </div>
