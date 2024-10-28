550935250d5b1971a4c026a8c53788ebdd9ac3bc
Joonwoo <liarcrown@naver.com>
2024-10-28 17:40:08 +0900

    design : last design

--- src/routes/profile/Story.tsx
+++ src/routes/profile/Story.tsx
@@ -2,7 +2,7 @@ import { PiExclamationMark } from "react-icons/pi";
 
 export default function Story() {
   return (
-    <div className="flex justify-center flex-col items-center h-full">
+    <div className="flex justify-center flex-col items-center h-full ">
       <div className="bg-[#efefef] w-12 h-12 flex justify-center items-center rounded-full mb-4">
         <PiExclamationMark size={32} color={"#7d7d7d"} />
       </div>
